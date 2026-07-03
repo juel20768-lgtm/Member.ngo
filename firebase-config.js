711b33b524ff771c4ba90f7166ae162480944258
juel20768-lgtm <juel20768@gmail.com>
2026-07-03 15:22:48 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,22 @@
-// Firebase SDK মডিউলসমূহ ইম্পোর্ট করা হলো
+// Firebase SDK মডিউলসমূহ CDN থেকে ইম্পোর্ট করা হলো
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// আপনার দেওয়া আসল ফায়ারবেস কনফিগারেশন
+// আপনার নতুন আপডেট করা ফায়ারবেস কনফিগারেশন
 const firebaseConfig = {
-  apiKey: "AIzaSyCPG0YQeTFcvK9CZdfArh5Yxhl9JJyPWKg",
-  authDomain: "ngocom-7a172.firebaseapp.com",
-  projectId: "ngocom-7a172",
-  storageBucket: "ngocom-7a172.firebasestorage.app",
-  messagingSenderId: "150547244077",
-  appId: "1:150547244077:web:cf0e2d36317ef5346a04a4",
-  measurementId: "G-Z1MB5NHE94"
+  apiKey: "AIzaSyCJqxLXJov5pHd8zHPi71xeFofFnVHfZO4",
+  authDomain: "ngo-3037d.firebaseapp.com",
+  projectId: "ngo-3037d",
+  storageBucket: "ngo-3037d.firebasestorage.app",
+  messagingSenderId: "730245140402",
+  appId: "1:730245140402:web:33a804f367f7be9e679eab",
+  measurementId: "G-7T6NTNYXC3"
 };
 
 // ফায়ারবেস ইনিশিয়ালাইজ করা হলো
 const app = initializeApp(firebaseConfig);
 
-// অথেনটিকেশন এবং ডাটাবেজ অবজেক্ট তৈরি ও এক্সপোর্ট
+// অথেনটিকেশন এবং ডাটাবেজ অবজেক্ট এক্সপোর্ট করা হলো যেন অন্যান্য ফাইল থেকে ব্যবহার করা যায়
 export const auth = getAuth(app);
 export const db = getFirestore(app);
